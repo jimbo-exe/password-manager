d3a009c17e1e5fb23e2e8c5a823b1d3c2f52138c
Aiden <sarwagya271@gmail.com>
2020-12-30 21:17:49 +0530

    Basic structure for tab functions

--- mainscreen.py
+++ mainscreen.py
@@ -3,11 +3,26 @@ import os
 import gpass as g
 import spass as s
 
+hideables = []
+
+
+def add_tab():
+    pass
+
+
+def retrieve_tab():
+    pass
+
+
+def edit_tab():
+    pass
+
 
 def show_welcome():
     global root
     global heading_label
     global tabs
+    global welcome_label
 
     os.chdir("bin")
     root = Tk()
@@ -33,16 +48,16 @@ def show_welcome():
                       bg="#505050",
                       foreground="#118ab2")
 
-    tabs = [tab_button("Retrieve Records"),
-            tab_button("Add New Record"),
-            tab_button("Edit Records")]
+    tabs = [tab_button("Retrieve Records").configure(command=retrieve_tab),
+            tab_button("Add New Record").configure(command=add_tab),
+            tab_button("Edit Records").configure(command=edit_tab)]
 
     for i in range(len(tabs)):
         tabs[i].grid(row=i + 1, column=0, sticky=W)
 
     welcome_label = Label(root,
-                          text="Welcome to Password Manager! Hope you having a great day!"+
-                           "Choose a tab from the left to get started.",
+                          text="Welcome to Password Manager! Hope you having a great day!" +
+                               "Choose a tab from the left to get started.",
                           font="Helvetica 15",
                           wraplength=500,
                           bg="#505050")
